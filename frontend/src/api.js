483269e16e1c6c783486961709696c296e9e5fba
Abdul5700 <abdulkhadar.i2006@gmail.com>
2026-07-14 19:23:12 +0530

    Fix API client

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,17 @@
-import axios from 'axios';export const api=axios.create({baseURL:import.meta.env.VITE_API_URL||'http://localhost:5000/api'});api.interceptors.request.use(c=>{const t=localStorage.getItem('token');if(t)c.headers.Authorization=`Bearer ${t}`;return c});
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
